3f6c8d1ef74a1c38247477961045abeb73272a09
Manish Saraswat <100990253+manish-121@users.noreply.github.com>
2023-07-27 11:18:35 +0530

    app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.sidebar.title("Olympics Analysis" )
 st.sidebar.image('https://e7.pngegg.com/pngimages/1020/402/png-clipart-2024-summer-olympics-brand-circle-area-olympic-rings-olympics-logo-text-sport.png')
 user_menu = st.sidebar.radio(
     'Select an Option',
-    ('Medal Tally', 'Overall Analysis', 'Country-wise Analysis')
+    ('Medal Tally', 'Overall Analysis', 'Country-wise Analysis','Athlete-wise Analysis')
 )
 
 
